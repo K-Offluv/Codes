4cc66b8f377dd52c6223739ba08d81d138c48033
K-Offluv <info@koffluvinvestmentlimited.com>
2025-11-25 13:28:28 +0000

    Files

---
+++ script.js
@@ -0,0 +1,15 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 60)
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  navbar.classList.toggle('open');
+  menu.classList.toggle('bx-x'); // Optional icon change
+};
+
+
